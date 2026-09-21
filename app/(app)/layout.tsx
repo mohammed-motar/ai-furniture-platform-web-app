92eb9b31f84f78c818ca1e87ba4378ca366ab9b8
Kaabi <kaabi.dev@gmail.com>
2026-09-21 16:01:55 +0200

    build header component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,12 +1,23 @@
+import { Header } from '@/components/Header';
+import { Toaster } from '@/components/ui/sonner';
+import { CartStoreProvider } from '@/lib/store/cart-store-provider';
+import { ChatStoreProvider } from '@/lib/store/chat-store-provider';
 import { SanityLive } from '@/sanity/lib/live';
 import { ClerkProvider } from '@clerk/nextjs';
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <main>{children}</main>
-      {/* Makes sanity realtime */}
-      <SanityLive />
+      <CartStoreProvider>
+        <ChatStoreProvider>
+          <Header />
+          <main>{children}</main>
+          <Toaster position='bottom-center' />
+
+          {/* Makes sanity realtime */}
+          <SanityLive />
+        </ChatStoreProvider>
+      </CartStoreProvider>
     </ClerkProvider>
   );
 }
